916463fc1814bea352139f8209ec756c4d9a1a2b
Jotis <juaneloyduranbon@hotmail.com>
2025-02-22 16:06:55 +0100

    Refactor file paths in rutas.py to use a base directory relative to the script and improve file existence checks

--- App/rutas.py
+++ App/rutas.py
@@ -1,38 +1,26 @@
 import os
 
-# Obtener la ruta absoluta del directorio actual
-current_dir = os.path.dirname(os.path.abspath(__file__))
+# Obtener la ruta base relativa al script actual
+base_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))  # Sube un nivel
 
-# Imprimir el directorio actual
-print(f"El directorio actual es: {current_dir}")
+# Definir las rutas de los archivos dentro de la carpeta "App"
+model_path = os.path.join(base_dir, 'App', 'alzheimer_model.h5')
+train_data_file_path = os.path.join(base_dir, 'App', 'train.parquet')
+test_data_file_path = os.path.join(base_dir, 'App', 'test.parquet')
+ml_model_path = os.path.join(base_dir, 'App', 'best_random_forest_model.pkl')
+feature_selector_path = os.path.join(base_dir, 'App', 'feature_selector.pkl')
+navigation_image_path = os.path.join(base_dir, 'App', 'image_3.jpeg')
+home_image_path = os.path.join(base_dir, 'App', 'image_2.jpeg')
+ml_report_path = os.path.join(base_dir, 'App', 'ML_Report.pdf')
+dl_report_path = os.path.join(base_dir, 'App', 'DL_Report.pdf')
 
-# Rutas de los archivos
-model_path = os.path.join(current_dir, 'alzheimer_model.h5')
-train_data_file_path = os.path.join(current_dir, 'train.parquet')
-test_data_file_path = os.path.join(current_dir, 'test.parquet')
-ml_model_path = os.path.join(current_dir, 'best_random_forest_model.pkl')
-feature_selector_path = os.path.join(current_dir, 'feature_selector.pkl')
-navigation_image_path = os.path.join(current_dir, 'image_3.jpeg')
-home_image_path = os.path.join(current_dir, 'image_2.jpeg')
-ml_report_path = os.path.join(current_dir, 'ML_Report.pdf')
-dl_report_path = os.path.join(current_dir, 'DL_Report.pdf')
-
-# Lista de rutas para comprobar
-paths = [
-    model_path,
-    train_data_file_path,
-    test_data_file_path,
-    ml_model_path,
-    feature_selector_path,
-    navigation_image_path,
-    home_image_path,
-    ml_report_path,
-    dl_report_path
-]
-
-# Comprobar si los archivos existen y mostrar la ruta
-for path in paths:
-    if os.path.exists(path):
-        print(f"El archivo existe: {path}")
-    else:
-        print(f"Archivo NO encontrado: {path}")
+# Verificar si las rutas son correctas
+print("Model Path:", model_path)
+print("Train Data Path:", train_data_file_path)
+print("Test Data Path:", test_data_file_path)
+print("ML Model Path:", ml_model_path)
+print("Feature Selector Path:", feature_selector_path)
+print("Navigation Image Path:", navigation_image_path)
+print("Home Image Path:", home_image_path)
+print("ML Report Path:", ml_report_path)
+print("DL Report Path:", dl_report_path)
